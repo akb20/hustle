6ef5fae243aa3166175836307e0ee2294b6a9e50
Aaron <aaronkbetts@gmail.com>
2016-03-13 09:57:52 +0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(function() {
+    $('.mobile-menu').on('click', function(e) {
+        e.stopPropagation();
+        $('nav').toggleClass('active').toggleClass('animated fadeIn');
+    });
+    $(window).scroll(function() {
+        var sc = $(window).scrollTop()
+        if (sc > 650) {
+            $(".logo").addClass("small")
+        } else {
+            $(".logo").removeClass("small")
+        }
+        if (sc > 650) {
+            $("header").addClass("transparent")
+        } else {
+            $("header").removeClass("transparent")
+        }
+        if (sc > 650) {
+            $(".nav-links").addClass("small")
+        } else {
+            $(".nav-links").removeClass("small")
+        }
+    });
+});
